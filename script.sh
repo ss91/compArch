7a0bf116e2f60751b31a638bed8433ee28f3561d
Sankalp <ss4728@columbia.edu>
2015-02-07 00:02:39 -0500

    modified script.sh

--- script.sh
+++ script.sh
@@ -1,25 +1,27 @@
 #! /bin/bash
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: bash $0 <# of integers> <C executable> <Java executable> <Python script> "
+if [ "$#" -ne 5 ]; then
+    echo "Usage: bash $0 <# of integers> <C executable_gcc> <Java executable> <Python script> <C_executable_clang> "
 	exit
 fi
 
-STR_C="c_output_$1"
+rm *10
+
+STR_C_GCC="gcc_output_$1"
 STR_JAVA="java_output_$1"
 STR_PYTHON="python_output_$1"
-
+STR_C_CLANG="clang_output_$1"
 for i in {1..10}
 do
 		
-	echo "Iteration $i" >> $STR_C
+	echo "Iteration $i" >> $STR_C_GCC
 	echo "Iteration $i" >> $STR_JAVA
 	echo "Iteration $i" >> $STR_PYTHON
-		
-	3>>$STR_C perf stat --log-fd 3 ./$2 $1 > /dev/null
+	echo "Iteration $i" >> $STR_C_CLANG	
+	3>>$STR_C_GCC perf stat --log-fd 3 ./$2 $1 > /dev/null
 	3>>$STR_JAVA perf stat --log-fd 3 java $3 $1 > /dev/null
 	3>>$STR_PYTHON perf stat --log-fd 3 python $4 $1 > /dev/null
-
+	3>>$STR_C_CLANG perf stat --log-fd 3 ./$5 $1 > /dev/null
 done
 
 	
